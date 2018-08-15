41a95ac3326fcde21f421e4491aedb420f1d0657
cherry <1048676598@qq.com>
2018-08-15 18:09:35 +0800

    git上传项目

---
+++ src/main/java/com/qjzd/network/service/BasInformationService.java
@@ -0,0 +1,18 @@
+package com.qjzd.network.service;
+
+import com.qjzd.network.dao.BasInformationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author:
+ * @Description:
+ * @Date Create on 18:07 2018/8/15
+ * @MOdifyBy:
+ * @parameter
+ */
+@Service
+public class BasInformationService {
+    @Autowired
+    private BasInformationMapper basInformationMapper;
+}
